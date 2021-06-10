591723251c59d8cb0daa1a5cc28e2a496f48cf43
Cesar Varela <mail@cesarvarela.com>
2021-06-10 01:08:48 -0300

    Improve connection lines

--- src/components/Connection.js
+++ src/components/Connection.js
@@ -3,20 +3,6 @@ import styled from 'styled-components'
 import { percentage } from '../lib/utils'
 import { Trash as TrashIcon } from 'grommet-icons'
 
-const Wrapper = styled.div`
-    position: absolute;
-    pointer-events: none;
-`
-
-
-
-const Point = styled.div`
-    width: 10px;
-    height: 10px;
-    background-color: #f00;
-    position: absolute;
-`
-
 const Trash = styled.div`
     position: absolute;
     top: 50%;
@@ -32,6 +18,33 @@ const Trash = styled.div`
     justify-content: center;
     pointer-events: auto;
     box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    border: 2px solid #ccc;
+`
+
+const Wrapper = styled.div`
+    position: absolute;
+    pointer-events: none;
+    min-width: 1%;
+    min-height: 1%;
+    svg {
+        position: absolute;
+        line {
+            stroke: #ccc;
+            stroke-width: 4;
+            position: absolute;
+        }
+    }
+    &:hover {
+        ${Trash} {
+            border: 2px solid #333;
+        }
+
+        svg {
+            line {
+                stroke: #333;
+            }
+        }
+    }
 `
 
 export default function Connection({ connections, from, to, onClick }) {
@@ -39,7 +52,7 @@ export default function Connection({ connections, from, to, onClick }) {
     const fromMiddle = { x: from.left + from.width / 2, y: from.top + from.height / 2 }
     const toMiddle = { x: to.left + to.width / 2, y: to.top + to.height / 2 }
 
-    const diagonal = (fromMiddle.x < toMiddle.x && fromMiddle.y < toMiddle.y) || (toMiddle.x < fromMiddle.x && toMiddle.y < fromMiddle.y)
+    let direction = (fromMiddle.x < toMiddle.x && fromMiddle.y < toMiddle.y) || (toMiddle.x < fromMiddle.x && toMiddle.y < fromMiddle.y)
         ? 'left' : 'right'
 
     const style = {
@@ -49,20 +62,40 @@ export default function Connection({ connections, from, to, onClick }) {
         height: percentage(Math.abs(toMiddle.y - fromMiddle.y)),
     }
 
-    return <Wrapper style={{ ...style }} diagonal={diagonal}>
-        {diagonal == 'left' &&
+    if (toMiddle.x == fromMiddle.x) {
+        direction = 'vertical'
+    }
+
+    if (toMiddle.y == fromMiddle.y) {
+        direction = 'horizontal'
+    }
+
+    return <Wrapper style={{ ...style }} diagonal={direction}>
+        {direction == 'left' &&
             <svg height="100%" width="100%">
-                <line x1="0" y1="0" x2="100%" y2="100%" style={{ stroke: 'rgb(255,0,0)', strokeWidth: 2, position: "absolute" }} />
+                <line x1="0" y1="0" x2="100%" y2="100%" />
             </svg>
         }
 
-        {diagonal == 'right' &&
+        {direction == 'right' &&
             <svg height="100%" width="100%">
-                <line x1="100%" y1="0" x2="0" y2="100%" style={{ stroke: 'rgb(255,0,0)', strokeWidth: 2, position: "absolute" }} />
+                <line x1="100%" y1="0" x2="0" y2="100%" />
             </svg>
         }
-        <Trash >
-            <TrashIcon className="icon" size="small" onClick={onClick} onClick={onClick} />
+
+        {direction == 'horizontal' &&
+            <svg height="100%" width="100%">
+                <line x1="0" y1="50%" x2="100%" y2="50%" />
+            </svg>
+        }
+
+        {direction == 'vertical' &&
+            <svg height="100%" width="100%">
+                <line x1="50%" y1="0" x2="50%" y2="100%" />
+            </svg>
+        }
+        <Trash onClick={onClick} >
+            <TrashIcon className="icon" size="small" />
         </Trash>
     </Wrapper>
 }
